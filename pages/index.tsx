398384c2eae3992f150ef95d7497fee3284c4976
Mckay Wrigley <mckaywrigley@gmail.com>
2023-03-03 13:18:39 -0700

    fix placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -294,7 +294,7 @@ export default function Home() {
                   ref={inputRef}
                   className="h-12 w-full rounded-full border border-zinc-600 pr-12 pl-11 focus:border-zinc-800 focus:outline-none focus:ring-1 focus:ring-zinc-800 sm:h-16 sm:py-2 sm:pr-16 sm:pl-16 sm:text-lg"
                   type="text"
-                  placeholder="How do I start a startup?"
+                  placeholder="What is the Instant Gratification Monkey?"
                   value={query}
                   onChange={(e) => setQuery(e.target.value)}
                   onKeyDown={handleKeyDown}
